ac82a7b7c919fd8979829adcc432672466427af7
Vladislav Kamenev <mazahakaforever@ya.ru>
2020-10-23 19:13:50 +0300

    Import command implemented

--- src/commands/import.ts
+++ src/commands/import.ts
@@ -1,11 +1,63 @@
-import Telegraf, {Context} from "telegraf";
-import adminMiddleware from "../middlewares/adminMiddleware";
-import { BotCommand } from "telegraf/typings/telegram-types";
+import Telegraf, { Context } from 'telegraf';
+import adminMiddleware from '../middlewares/adminMiddleware';
+import { BotCommand } from 'telegraf/typings/telegram-types';
+import fetch from 'node-fetch';
+import { OldExtraModel, NewExtraModel, ExtraModel } from '../models';
+
+const { MASTER_ID } = process.env;
 
 export default (bot: Telegraf<Context>, commands: Array<BotCommand>) => {
     bot.command('import', adminMiddleware, async (ctx: Context) => {
-        ctx.reply(`Not implemented. Please donate a шаверма`);
+        if (!ctx.message.reply_to_message.document) {
+            return ctx.reply('Where is the backup?');
+        }
+
+        try {
+            const document = ctx.message.reply_to_message.document;
+            const downloadLink = await ctx.telegram.getFileLink(document.file_id);
+            const response = await fetch(downloadLink);
+            const contents = await response.json();
+            for (const chatId of Object.keys(contents)) {
+                if (parseInt(chatId) !== ctx.chat.id && ctx.chat.id !== parseInt(MASTER_ID)) {
+                    await ctx.reply(`This backup of other chat`);
+                }
+                const extras: Record<string, string> = contents[chatId];
+
+                for (const [hashtag, data] of Object.entries(extras)) {
+                    const oldExtra = await ExtraModel.findOne({
+                        hashtag,
+                        chat: chatId,
+                    });
+
+                    if (oldExtra) {
+                        console.log(`${hashtag} deleted before backup replace`);
+                        await ExtraModel.deleteOne({ hashtag, chat: chatId });
+                    }
+
+                    const parsedData = JSON.parse(data);
+                    if (parsedData.kind === 'Old') {
+                        await new OldExtraModel({
+                            chat: chatId,
+                            hashtag,
+                            code: parsedData.code,
+                        }).save();
+                    } else {
+                        await new NewExtraModel({
+                            chat: chatId,
+                            hashtag,
+                            type: parsedData.type,
+                            replica: parsedData.replica,
+                            description: parsedData.description || null,
+                        }).save();
+                    }
+                }
+                await ctx.reply(`Chat: ${chatId}. Restored extras: ${Object.entries(extras).length}`);
+            }
+        } catch (e) {
+            console.error(e);
+            return ctx.reply('Shit happened');
+        }
     });
 
     commands.push({ command: 'import', description: 'Import an chat backup' });
-}
+};
