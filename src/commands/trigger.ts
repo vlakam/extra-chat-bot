7ddc3028192defdda9107ca7c2c9581bff229fd0
Sergey <iceman9831@gmail.com>
2020-10-24 13:55:55 +0300

    [WIP] Suggestions implementation

--- src/commands/trigger.ts
+++ src/commands/trigger.ts
@@ -1,8 +1,9 @@
 import Telegraf, {Context} from "telegraf";
+import Markup from "telegraf/markup";
 import {ExtraModel, INewExtra, IOldExtra, OldExtraModel, NewExtraModel} from "../models";
 import replicators from 'telegraf/core/replicators';
 import report from "../helpers/report";
-
+import difflib from "difflib";
 
 
 const handleNewExtras = async (extra: INewExtra, ctx: Context) => {
@@ -57,7 +58,55 @@ const setupExtraTrigger = (bot: Telegraf<Context>) => {
             } catch (e) {
                 report(`Failed to send extra on ${hashtag}. Err: ${e}`);
             }
-        }
+        } else {
+            const extrasInChat = await ExtraModel.find({
+                chat: id,
+            });
+            let hashtagsInChat = [];
+            for (let extraItem of extrasInChat) {
+                hashtagsInChat.push(extraItem.hashtag);
+            }
+
+            if (hashtagsInChat.length > 0) {
+                const closeMatches = difflib.getCloseMatches(hashtag, hashtagsInChat);
+                if (closeMatches.length > 0) {
+                    const slicedSuggestions = closeMatches.slice(0, 4);
+                    
+                    let suggestions = []; // type - extra (json)
+                    
+                    for (let item of slicedSuggestions) {
+                        for (let jsonItem of extrasInChat) {
+                            if (jsonItem.hashtag === item) {
+                                suggestions.push(jsonItem);
+                                break;
+                            }
+                        }
+                    }
+
+                    let keyboard = [];
+                    
+                    for (let item of suggestions) {
+                        let buttonName = item.hashtag;
+                        if (item.description) {
+                            buttonName = `${buttonName} - ${item.description.slice(0, 140)}`
+                        }
+                        keyboard.push(Markup.callbackButton(buttonName, `suggestion:${item.hashtag}`));
+                    }
+                    await ctx.telegram.sendMessage(id, `Unknown extra <code>${hashtag}</code>. Maybe you meant`, { parse_mode: "HTML", reply_markup: Markup.inlineKeyboard(keyboard, {columns: 1}) });
+                }
+            }
+        } 
+    });
+
+    bot.action(/^suggestion:#?([^\s]+)$/, async (ctx: Context) => {
+        let hashtag = `#${ctx.match[1]}`;
+        const {id} = ctx.callbackQuery.message.chat;
+
+        console.log(`suggestion ${hashtag}`);
+
+        // Here we must reuse function from above. Need a small refactor.
+
+        await ctx.answerCbQuery();
     });
 };
 
